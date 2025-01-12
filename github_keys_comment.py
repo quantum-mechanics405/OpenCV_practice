bafc4e54941d27a0687bf2e680ea97ac58e221a5
Muhammad Umar <akbin5778@gmail.com>
2025-01-12 10:03:23 +0500

    adding hello world

--- github_keys_comment.py
+++ github_keys_comment.py
@@ -1,14 +1,3 @@
-# @echo off
-# REM Ensure we are in the correct Git repository directory
-# cd /d "C:\Users\DELL\Desktop\Computer vision\openCV"
-
-# REM Git commands to add, commit, and push changes
-# git add .
-# git commit -m "Automated commit: adding new line"
-# git push https://quantum-mechanics405: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@github.com/quantum-mechanics405/OpenCV_practice.git main
-
-# pause
-# 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 #old one
-# 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  #New one 12 jan 2025
+# hi/
  
  
